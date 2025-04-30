e89c371b5420c2d3737254f349e78af583380540
dominators47 <1452487976@qq.com>
2025-04-30 12:51:02 +0800

    添加 IntList、IntNode、SLList 和 PassByValueFigure 类的初始实现

---
+++ IntList.java
@@ -0,0 +1,16 @@
+public class IntList {
+    public int first;
+    public IntList rest;        
+
+    public static void main(String[] args) {
+        IntList L = new IntList();
+        L.first = 5;
+        L.rest = null;
+
+        L.rest = new IntList();
+        L.rest.first = 10;
+
+        L.rest.rest = new IntList();
+        L.rest.rest.first = 15;
+    }
+}
---
+++ SLList.java
@@ -0,0 +1,15 @@
+public class SLList {
+    public IntNode first;
+
+    public SLList(int x) {
+        first = new IntNode(x, null);
+    }
+
+    /** Adds an item to the front of the list. */
+    public void addFirst(int x) {
+        first = new IntNode(x, first);
+    }
+    public int getFirst(){
+        return first.item;
+    }
+}
