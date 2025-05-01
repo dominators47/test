42221c34f65da79dbe565ccc1ef377928ad74073
dominators47 <1452487976@qq.com>
2025-05-01 14:39:42 +0800

    完善 SLList 类，添加 addlast 和 size 方法

--- SLList.java
+++ SLList.java
@@ -10,9 +10,11 @@ public class SLList {
         }
     }
     private IntNode first;
+    private  int  size;
 
     public SLList(int x) {
         first = new IntNode(x, null);
+        size = 1;
     }
 
     public void addfirst(int x){
@@ -21,10 +23,27 @@ public class SLList {
     public int getfirst(){
         return first.item;
     }
+    public void addlast(int x){
+        IntNode p = first;
+        while(p.next != null){
+            p = p.next;
+        }
+        p.next = new IntNode(x, null);
+    }
+    private static int size(IntNode p){
+        if (p.next == null){
+            return 1;
+        }
+        return 1 + size(p.next);
+    }
+    public int size(){
+        return size(first);
+    }
     public static void main(String[] args) {
         SLList L = new SLList(15);
         L.addfirst(10);
         L.addfirst(5);
-        System.out.println(L.getfirst());
+        L.addlast(20);
+        System.out.println(L.size());
     }
     }
